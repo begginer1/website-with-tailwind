7b7e2c42d6fe770d169ade5de4cc8d08c8056cdb
begginer1 <sachinsajwan7@gmail.com>
2024-04-08 12:34:32 +0530

    tailwind and nav bar partially comeleted

---
+++ script.js
@@ -0,0 +1,6 @@
+let navDialog=document.getElementById('nav-dialog');
+function handleMenu()
+{
+    console.log("button clicked")
+    navDialog.classList.toggle("hidden")
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:
+      {
+        primary:'#3238f2'
+      },
+      fontFamily:
+      {
+        sachin:['Poppins','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
